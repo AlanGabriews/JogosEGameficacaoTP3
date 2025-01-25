9b9e80e202c8ae071c041c5344ae7bb7634d3f31
Leokto <luiz.galvao@ufv.br>
2025-01-25 09:51:48 -0300

    Implementado as colisões dos rockets 1 e 2 para os jogadores 1 e 2

---
+++ War battles tutorial/main/rocket1.go
@@ -0,0 +1,46 @@
+components {
+  id: "rocket"
+  component: "/main/rocket.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"rocket\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/sprites.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 3.0
+    y: 3.0
+    z: 3.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"rocket1\"\n"
+  "mask: \"tank2\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 4.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 21.655107\n"
+  "  data: 9.882542\n"
+  "  data: 13.04\n"
+  "}\n"
+  ""
+}
